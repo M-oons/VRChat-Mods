28fa00456a6cef0b8893c5ed120ed00672330be3
M-oons <qlowds@gmail.com>
2020-09-09 12:50:00 +0100

    Create Utils.cs

---
+++ ComfyVRMenu/Utils.cs
@@ -0,0 +1,103 @@
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace ComfyVRMenu
+{
+    public static class Utils
+    {
+        public static bool IsInVR()
+        {
+            return XRDevice.isPresent;
+        }
+
+        public static VRCPlayer GetVRCPlayer()
+        {
+            return VRCPlayer.field_Internal_Static_VRCPlayer_0;
+        }
+
+        public static VRCTrackingManager GetVRCTrackingManager()
+        {
+            return VRCTrackingManager.field_Private_Static_VRCTrackingManager_0;
+        }
+
+        public static VRCVrCamera GetVRCVrCamera()
+        {
+            return VRCVrCamera.field_Private_Static_VRCVrCamera_0;
+        }
+
+        public static Vector3 GetWorldCameraPosition()
+        {
+            VRCVrCamera camera = GetVRCVrCamera();
+            var type = camera.GetIl2CppType();
+            if (type == Il2CppType.Of<VRCVrCameraSteam>())
+            {
+                VRCVrCameraSteam steam = camera.Cast<VRCVrCameraSteam>();
+                Transform transform1 = steam.field_Private_Transform_0;
+                Transform transform2 = steam.field_Private_Transform_1;
+                if (transform1.name == "Camera (eye)")
+                {
+                    return transform1.position;
+                }
+                else if (transform2.name == "Camera (eye)")
+                {
+                    return transform2.position;
+                }
+            }
+            else if (type == Il2CppType.Of<VRCVrCameraUnity>())
+            {
+                VRCVrCameraUnity unity = camera.Cast<VRCVrCameraUnity>();
+                return unity.CameraStereo.transform.position;
+            }
+            else if (type == Il2CppType.Of<VRCVrCameraWave>())
+            {
+                VRCVrCameraWave wave = camera.Cast<VRCVrCameraWave>();
+                return wave.transform.position;
+            }
+            return camera.transform.parent.TransformPoint(GetLocalCameraPosition());
+        }
+
+        public static Vector3 GetLocalCameraPosition()
+        {
+            VRCVrCamera camera = GetVRCVrCamera();
+            var type = camera.GetIl2CppType();
+            if (type == Il2CppType.Of<VRCVrCameraGoogle>())
+            {
+                return camera.transform.localPosition;
+            }
+            else if (type == Il2CppType.Of<VRCVrCameraSteam>())
+            {
+                VRCVrCameraSteam steam = camera.Cast<VRCVrCameraSteam>();
+                Transform transform1 = steam.field_Private_Transform_0;
+                Transform transform2 = steam.field_Private_Transform_1;
+                if (transform1.name == "Camera (eye)")
+                {
+                    return camera.transform.parent.InverseTransformPoint(transform1.position);
+                }
+                else if (transform2.name == "Camera (eye)")
+                {
+                    return camera.transform.parent.InverseTransformPoint(transform2.position);
+                }
+                else
+                {
+                    return Vector3.zero;
+                }
+            }
+            else if (type == Il2CppType.Of<VRCVrCameraUnity>())
+            {
+                if (IsInVR())
+                {
+                    return camera.transform.localPosition + InputTracking.GetLocalPosition(XRNode.CenterEye);
+                }
+                VRCVrCameraUnity unity = camera.Cast<VRCVrCameraUnity>();
+                return camera.transform.parent.InverseTransformPoint(unity.CameraStereo.transform.position);
+            }
+            else if (type == Il2CppType.Of<VRCVrCameraWave>())
+            {
+                VRCVrCameraWave wave = camera.Cast<VRCVrCameraWave>();
+                return wave.trackingRoot.InverseTransformPoint(camera.transform.position);
+            }
+            return Vector3.zero;
+        }
+    }
+}
