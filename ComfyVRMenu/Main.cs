8e81e09aa3dff52b7f60ea902e4fda0f4dd430a4
M-oons <qlowds@gmail.com>
2021-02-02 21:52:38 +0000

    Update Main.cs

--- ComfyVRMenu/Main.cs
+++ ComfyVRMenu/Main.cs
@@ -9,33 +9,33 @@ namespace ComfyVRMenu
     public static class ModInfo
     {
         public const string NAME = "ComfyVRMenu";
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.0.1";
     }
 
     public class Main : MelonMod
     {
         public override void OnApplicationStart()
         {
-            MelonPrefs.RegisterCategory("ComfyVRMenu", "Comfy VR Menu");
-            MelonPrefs.RegisterBool("ComfyVRMenu", "EnableComfyVRMenu", true, "Enable Comfy VR Menu");
+            MelonPreferences.CreateCategory("ComfyVRMenu", "Comfy VR Menu");
+            MelonPreferences.CreateEntry("ComfyVRMenu", "EnableComfyVRMenu", true, "Enable Comfy VR Menu");
 
-            _comfyVRMenu = MelonPrefs.GetBool("ComfyVRMenu", "EnableComfyVRMenu");
+            _comfyVRMenu = MelonPreferences.GetEntryValue<bool>("ComfyVRMenu", "EnableComfyVRMenu");
 
             var harmony = HarmonyInstance.Create("ComfyVRMenu");
 
             var method = PlaceUiMethod;
             if (method == null)
             {
-                MelonLogger.LogError("Couldn't find VRCUiManager PlaceUi method to patch.");
+                MelonLogger.Error("Couldn't find VRCUiManager PlaceUi method to patch.");
                 return;
             }
 
             harmony.Patch(typeof(VRCUiManager).GetMethod(method.Name), GetPatch(nameof(Main.PlaceUiPatch)));
         }
 
-        public override void OnModSettingsApplied()
+        public override void OnPreferencesSaved()
         {
-            _comfyVRMenu = MelonPrefs.GetBool("ComfyVRMenu", "EnableComfyVRMenu");
+            _comfyVRMenu = MelonPreferences.GetEntryValue<bool>("ComfyVRMenu", "EnableComfyVRMenu");
         }
 
         private static HarmonyMethod GetPatch(string name)
@@ -45,53 +45,56 @@ namespace ComfyVRMenu
 
         private static bool PlaceUiPatch(VRCUiManager __instance, bool __0)
         {
-            if (!Utils.IsInVR() || !_comfyVRMenu) return true;
-            float scale = Utils.GetVRCTrackingManager() != null ? Utils.GetVRCTrackingManager().transform.localScale.x : 1f;
-            if (scale <= 0f)
+            if (!Utils.IsInVR()) return true;
+            float num = Utils.GetVRCTrackingManager() != null ? Utils.GetVRCTrackingManager().transform.localScale.x : 1f;
+            if (num <= 0f)
             {
-                scale = 1f;
+                num = 1f;
             }
-            __instance.playerTrackingDisplay.position = Utils.GetWorldCameraPosition();
+            var playerTrackingDisplay = __instance.transform;
+            var unscaledUIRoot = __instance.transform.Find("UnscaledUI");
+            playerTrackingDisplay.position = Utils.GetWorldCameraPosition();
             Vector3 rotation = GameObject.Find("Camera (eye)").transform.rotation.eulerAngles;
             Vector3 euler = new Vector3(rotation.x - 30f, rotation.y, 0f);
+            //if (rotation.x > 0f && rotation.x < 300f) rotation.x = 0f;
             if (Utils.GetVRCPlayer() == null)
             {
                 euler.x = euler.z = 0f;
             }
             if (!__0)
             {
-                __instance.playerTrackingDisplay.rotation = Quaternion.Euler(euler);
+                playerTrackingDisplay.rotation = Quaternion.Euler(euler);
             }
             else
             {
                 Quaternion quaternion = Quaternion.Euler(euler);
-                if (!(Quaternion.Angle(__instance.playerTrackingDisplay.rotation, quaternion) < __instance.QuantizedAngleDegrees))
+                if (!(Quaternion.Angle(playerTrackingDisplay.rotation, quaternion) < 15f))
                 {
-                    if (!(Quaternion.Angle(__instance.playerTrackingDisplay.rotation, quaternion) < __instance.MaxAngleDegrees))
+                    if (!(Quaternion.Angle(playerTrackingDisplay.rotation, quaternion) < 25f))
                     {
-                        __instance.playerTrackingDisplay.rotation = Quaternion.RotateTowards(__instance.playerTrackingDisplay.rotation, quaternion, __instance.MaxDeltaDegreesFast);
+                        playerTrackingDisplay.rotation = Quaternion.RotateTowards(playerTrackingDisplay.rotation, quaternion, 5f);
                     }
                     else
                     {
-                        __instance.playerTrackingDisplay.rotation = Quaternion.RotateTowards(__instance.playerTrackingDisplay.rotation, quaternion, __instance.MaxDeltaDegrees);
+                        playerTrackingDisplay.rotation = Quaternion.RotateTowards(playerTrackingDisplay.rotation, quaternion, 1f);
                     }
                 }
             }
-            if (scale >= 0f)
+            if (num >= 0f)
             {
-                __instance.playerTrackingDisplay.localScale = scale * Vector3.one;
+                playerTrackingDisplay.localScale = num * Vector3.one;
             }
             else
             {
-                __instance.playerTrackingDisplay.localScale = Vector3.one;
+                playerTrackingDisplay.localScale = Vector3.one;
             }
-            if (scale > float.Epsilon)
+            if (num > float.Epsilon)
             {
-                __instance.unscaledUIRoot.localScale = 1f / scale * Vector3.one;
+                unscaledUIRoot.localScale = 1f / num * Vector3.one;
             }
             else
             {
-                __instance.unscaledUIRoot.localScale = Vector3.one;
+                unscaledUIRoot.localScale = Vector3.one;
             }
             return false;
         }
